1a58ea64e7c6df6341fbc1e59c74e8639d25e70b
Ariel Ignacio Curtido García <aicurtido@gmail.com>
2023-12-01 22:33:32 -0300

    Cambio de icono

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 
 import styles from './styles.module.css'
 import imgSun from '/images/icon-sun.svg'
+import imgMoon from '/images/icon-moon.svg'
 
 export default function Title() {
+  const [tema, setTema] = React.useState('dark') 
+
+  const cambiarTema = () => {
+    if(tema === 'dark'){
+      setTema('light')
+    }else{
+      setTema('dark')
+    }
+  }
+
   return (
     <div className={styles.container}>
         <h1 className={styles.todoText}>TODO</h1>
-        <img className={styles.sunImg} src={imgSun} alt="" />
+        <img onClick={cambiarTema}  className={styles.sunImg} src={tema == 'dark' ? imgSun : imgMoon } alt="" />
     </div>
   )
 }
